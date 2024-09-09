7c80466193d1ec1227c97fbfc41e885f30d1ea05
gabrielcode-7 <gabriel22tim@gmail.com>
2024-09-08 22:05:43 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados =  atletas = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com o seu talento e carisma. Aos poucos anos ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags:"skate fada sk8"
+         
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é uma das maiores promessas do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags:"judo judô judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira e mundial. Com sua força, determinação e talento inigualáveis, ela conquistou o coração dos brasileiros e se tornou um verdadeiro símbolo de superação.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags:"ginástica ginastica ouro"
+    }
+];
+
